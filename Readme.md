c3526b259f3b7904c1a87a894c97dd4b5c1ff5dd
Santa Claus <santa@northpole.com>
2018-12-21 11:20:38 +0200

    add how to instal softwere

--- Readme.md
+++ Readme.md
@@ -15,3 +15,7 @@ Security: Malwarebytes Anti-Malware.
 
 ## Installing
 How to install software
+Locate and download an .exe file.
+Locate and double-click the .exe file. (It will usually be in your Downloads folder.)
+A dialog box will appear. Follow the instructions to install the software.
+The software will be installed.
--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function mytest()
 {
 var q1=document.forms["myform"]["flavor1"].value;
-var q2=document.forms["myform"]["answer2"].value;
+var q2=document.forms["myform"]["flavor2"].value;
 var q3=document.forms["myform"]["flavor3"].value;
 var q4=document.forms["myform"]["flavor4"].value;
 var q5=document.forms["myform"]["flavor5"].value;
@@ -20,37 +20,34 @@ if(q2=="JavaScript is designed for creating network-centric applications")
 {
   marks=marks+2;
   }
-if(q3=="The head section")
+if(q3==" Less server interaction")
 {
   marks=marks+2;
 }
-if(q4=="Microsoft")
+if(q4==" false")
 {
   marks=marks+2;
 }
-if(q5=="true")
+if(q5==" true")
 {
   marks=marks+2;
 }
-if(q6=="alertBox Hello World")
+if(q6=="characterAt()")
 {
   marks=marks+2;
   }
-if(q7=="function = myFunction()")
+if(q7=="append()")
 {
   marks=marks+2;
   }
-if(q8=="call myFunction()")
+if(q8==" global variable")
 {
   marks=marks+2;
 }
-if(q9=="if i = 5")
+if(q9==" None of the above.")
 {
   marks=marks+2;
 }
-if(q10==" if (i != 5)")
-{
-  marks=marks+2;
-}
-alert("your score is:"+marks);
-}
+    alert("your score is:"+marks);
+
+ }
