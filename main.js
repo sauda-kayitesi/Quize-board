dc80de172a69b859511957392c76e82397be71c2
Santa Claus <santa@northpole.com>
2018-12-21 10:47:10 +0200

    add answer

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
 function mytest()
 {
-var q1=document.forms["myform"]["answer1"].value;
+var q1=document.forms["myform"]["flavor1"].value;
 var q2=document.forms["myform"]["answer2"].value;
-var q3=document.forms["myform"]["answer3"].value;
-var q4=document.forms["myform"]["answer4"].value;
-var q5=document.forms["myform"]["answer5"].value;
-var q6=document.forms["myform"]["answer6"].value;
-var q7=document.forms["myform"]["answer7"].value;
-var q8=document.forms["myform"]["answer8"].value;
-var q9=document.forms["myform"]["answer9"].value;
+var q3=document.forms["myform"]["flavor3"].value;
+var q4=document.forms["myform"]["flavor4"].value;
+var q5=document.forms["myform"]["flavor5"].value;
+var q6=document.forms["myform"]["flavor6"].value;
+var q7=document.forms["myform"]["flavor7"].value;
+var q8=document.forms["myform"]["flavor8"].value;
+var q9=document.forms["myform"]["flavor9"].value;
 
 var marks=0;
-if(q1=="script")
+if(q1=="All of the above.")
 {
   marks=marks+2;
   }
-if(q2=="Hypertext markup language")
+if(q2=="JavaScript is designed for creating network-centric applications")
 {
   marks=marks+2;
   }
